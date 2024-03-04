c81808daaa99fbd25446ae57beb5f0bdb55a681e
Alex McGinn <tug74324@temple.edu>
2024-03-04 10:21:08 -0500

    Added Microsoft Entity Framework for MSSQL server.

---
+++ MerchStore.Api/Data/MerchStoreContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MerchStore.Api.Data;
+
+/*represents a session with the db and can be
+  used to query and set instances of our entities*/
+public class MerchStoreContext: DbContext
+{
+
+    
+}
